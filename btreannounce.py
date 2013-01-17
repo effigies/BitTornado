6fd3396538a5870ca584039438905e5afead74e4
Chris Johnson <effigies@gmail.com>
2013-01-17 18:35:49 -0500

    Rewrite btreannounce as function and wrapper

--- btreannounce.py
+++ btreannounce.py
@@ -4,76 +4,77 @@
 # multitracker extensions by John Hoffman
 # see LICENSE.txt for license information
 
-from sys import argv,exit
-from os.path import split
+import sys
+import os
+import getopt
 from BitTornado.bencode import bencode, bdecode
+from BitTornado.BT1.makemetafile import announcelist_details
 
-if len(argv) < 3:
-    a,b = split(argv[0])
-    print ('Usage: ' + b + ' <announce> [--announce_list <arg>] file1.torrent [file2.torrent...]')
-    print ('')
-    print ('  Where:')
-    print ('    announce = tracker URL')
-    print ('           Example: http://www.tracker.com:6699/announce')
-    print ('')
-    print ('    announce_list = optional list of redundant/backup tracker URLs, in the format:')
-    print ('           url[,url...][|url[,url...]...]')
-    print ('                where URLs separated by commas are all tried first')
-    print ('                before the next group of URLs separated by the pipe is checked.')
-    print ("                If none is given, it is assumed you don't want one in the metafile.")
-    print ('                If announce-list is given, clients which support it')
-    print ('                will ignore the <announce> value.')
-    print ('           Examples:')
-    print ('                http://tracker1.com|http://tracker2.com|http://tracker3.com')
-    print ('                     (tries trackers 1-3 in order)')
-    print ('                http://tracker1.com,http://tracker2.com,http://tracker3.com')
-    print ('                     (tries trackers 1-3 in a randomly selected order)')
-    print ('                http://tracker1.com|http://backup1.com,http://backup2.com')
-    print ('                     (tries tracker 1 first, then tries between the 2 backups randomly)')
-    print ('')
-    exit(2) # common exit code for syntax error
+announce_details = """
+  Where:
+    announce = tracker URL
+        Example: http://www.tracker.com:6699/announce
+"""
 
-announce = argv[1]
-announce_list = []
-if argv[2] == '--announce_list':
-    for tier in argv[3].split('|'):
-        sublist = []
-        for tracker in tier.split(','):
-            sublist += [tracker]
-        announce_list += [sublist]
-    if len(argv) < 5:
-        print ('error: no .torrent files given')
-        print ('')
-        exit(2)
-    argv = argv[2:]
+def reannounce(fname, announce, announce_list = None, verbose = False):
+    metainfo_file = open(fname, 'rb')
+    metainfo = bdecode(metainfo_file.read())
+    metainfo_file.close()
+
+    if verbose:
+        print 'old announce for %s: %s' % (fname, metainfo['announce'])
     
-
-for f in argv[2:]:
-    h = open(f, 'rb')
-    metainfo = bdecode(h.read())
-    h.close()
-    print 'old announce for %s: %s' % (f, metainfo['announce'])
     metainfo['announce'] = announce
+    
     if metainfo.has_key('announce-list'):
-        list = []
-        for tier in metainfo['announce-list']:
-            for tracker in tier:
-                list+=[tracker,',']
-            del list[-1]
-            list+=['|']
-        del list[-1]
-        liststring = ''
-        for i in list:
-            liststring+=i
-        print 'old announce-list for %s: %s' % (f, liststring)
-    if len(announce_list) > 0:
-        metainfo['announce-list'] = announce_list
-    elif metainfo.has_key('announce-list'):
-        try:
-            del metainfo['announce-list']
-        except:
-            pass
-        
-    h = open(f, 'wb')
-    h.write(bencode(metainfo))
-    h.close()
+        if verbose:
+            print 'old announce-list for %s: %s' % (fname,
+                '|'.join(','.join(tier) for tier in metainfo['announce-list']))
+        if announce_list is not None:
+            metainfo['announce-list'] = announce_list
+        else:
+            try:
+                del metainfo['announce-list']
+            except:
+                pass
+            
+    metainfo_file = open(fname, 'wb')
+    metainfo_file.write(bencode(metainfo))
+    metainfo_file.close()
+
+def main(argv):
+    program, ext = os.path.splitext(os.path.basename(argv[0]))
+    usage = "Usage: %s <announce> [--announce_list <arg>] " \
+            "file1.torrent [file2.torrent...]" % program
+    help =  "%s\n%s\n%s" % (usage, announce_details, '\n'.join(' ' * 4 + l
+            for l in announcelist_details.split('\n')[:-2]))
+
+    try:
+        opts, args = getopt.getopt(argv[1:], "hav",
+                        ("help","announce_list","verbose"))
+    except getopt.error, msg:
+        print msg
+        return 1
+
+    if len(args) < 2:
+        print help
+        return 2
+    
+    announce        = args[0]
+    announce_list   = None
+    verbose         = False
+
+    for opt, arg in opts:
+        if opt in ('-h','--help'):
+            print help
+            return 0
+        elif opt in ('-a','--announce_list'):
+            announce_list = [tier.split(',') for tier in arg.split('|')]
+        elif opt in ('-v','--verbose'):
+            verbose = True
+
+    for fname in args[1:]:
+        reannounce(fname, announce, announce_list, verbose)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))

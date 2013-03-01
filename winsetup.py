73b0f87aa58faed8e7be8a3044b2a2346933e2bd
Chris Johnson <effigies@gmail.com>
2013-03-01 01:02:38 -0500

    STY: PEP8/pyflakes clean winsetup

--- winsetup.py
+++ winsetup.py
@@ -8,9 +8,9 @@ from distutils.core import setup
 import py2exe
 
 setup(
-    windows = [ { 'script': 'btdownloadgui.py',
-                  'icon_resources': [ (1, 'icon_bt.ico')],
-                    'excludes': ["pywin", "pywin.debugger", "pywin.debugger.dbgcon",
-                "pywin.dialogs", "pywin.dialogs.list",
-                "Tkconstants","Tkinter","tcl" ]  } ]
-    )
+    windows=[{'script': 'btdownloadgui.py',
+              'icon_resources': [(1, 'icon_bt.ico')],
+              'excludes': ["pywin", "pywin.debugger", "pywin.debugger.dbgcon",
+                           "pywin.dialogs", "pywin.dialogs.list",
+                           "Tkconstants", "Tkinter", "tcl"]}]
+)

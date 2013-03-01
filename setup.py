49ecc0693d4e17ad7411b1b26e97154f5f903095
Chris Johnson <effigies@gmail.com>
2013-03-01 01:00:08 -0500

    STY: PEP8/pyflakes clean setup

--- setup.py
+++ setup.py
@@ -3,25 +3,23 @@
 # Written by Bram Cohen
 # see LICENSE.txt for license information
 
-import sys
-from distutils.core import setup, Extension
+from distutils.core import setup
 import BitTornado
 
 setup(
-    name = "BitTornado",
-    version = BitTornado.version,
-    author = "Bram Cohen, John Hoffman, Uoti Arpala et. al.",
-    author_email = "<theshadow@degreez.net>",
-    url = "http://www.bittornado.com",
-    license = "MIT",
-    
-    packages = ["BitTornado","BitTornado.BT1"],
+    name="BitTornado",
+    version=BitTornado.version,
+    author="Bram Cohen, John Hoffman, Uoti Arpala et. al.",
+    author_email="<theshadow@degreez.net>",
+    url="http://www.bittornado.com",
+    license="MIT",
 
-    scripts = ["btdownloadgui.py", "btdownloadheadless.py", 
-        "bttrack.py", "btmakemetafile.py", "btlaunchmany.py", "btcompletedir.py",
-        "btdownloadcurses.py", "btcompletedirgui.py", "btlaunchmanycurses.py", 
-        "btmakemetafile.py", "btreannounce.py", "btrename.py", "btshowmetainfo.py",
-        'btmaketorrentgui.py', 'btcopyannounce.py', 'btsethttpseeds.py',
-        'bt-t-make.py',
-        ]
-    )
+    packages=["BitTornado", "BitTornado.BT1"],
+
+    scripts=["btdownloadgui.py", "btdownloadheadless.py", "bttrack.py",
+             "btmakemetafile.py", "btlaunchmany.py", "btcompletedir.py",
+             "btdownloadcurses.py", "btcompletedirgui.py",
+             "btlaunchmanycurses.py", "btmakemetafile.py", "btreannounce.py",
+             "btrename.py", "btshowmetainfo.py", 'btmaketorrentgui.py',
+             'btcopyannounce.py', 'btsethttpseeds.py', 'bt-t-make.py']
+)

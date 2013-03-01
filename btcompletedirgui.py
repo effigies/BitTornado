c4ddd0fe49fedc954ca7f0bdeadfb4d7ba1f6d04
Chris Johnson <effigies@gmail.com>
2013-03-01 02:59:14 -0500

    STY: Repack wx.* into the wx namespace in btcompletedirgui

--- btcompletedirgui.py
+++ btcompletedirgui.py
@@ -8,21 +8,21 @@ import os
 import threading
 from BitTornado.BT1.makemetafile import completedir
 try:
-    from wxPython.wx import *
+    from wxPython import wx
 except:
     print 'wxPython is not installed or has not been installed properly.'
     sys.exit(1)
 
-wxEVT_INVOKE = wxNewEventType()
+wxEVT_INVOKE = wx.wxNewEventType()
 
 
 def EVT_INVOKE(win, func):
     win.Connect(-1, -1, wxEVT_INVOKE, func)
 
 
-class InvokeEvent(wxPyEvent):
+class InvokeEvent(wx.wxPyEvent):
     def __init__(self, func, args, kwargs):
-        wxPyEvent.__init__(self)
+        super(InvokeEvent, self).__init__()
         self.SetEventType(wxEVT_INVOKE)
         self.func = func
         self.args = args
@@ -31,32 +31,33 @@ class InvokeEvent(wxPyEvent):
 
 class DownloadInfo:
     def __init__(self):
-        frame = wxFrame(None, -1, 'BitTorrent complete dir 1.0.1',
-                        size=wxSize(550, 250))
+        frame = wx.wxFrame(None, -1, 'BitTorrent complete dir 1.0.1',
+                           size=wx.wxSize(550, 250))
         self.frame = frame
 
-        panel = wxPanel(frame, -1)
+        panel = wx.wxPanel(frame, -1)
 
-        gridSizer = wxFlexGridSizer(cols=2, rows=2, vgap=15, hgap=8)
+        gridSizer = wx.wxFlexGridSizer(cols=2, rows=2, vgap=15, hgap=8)
 
-        gridSizer.Add(wxStaticText(panel, -1, 'directory to build:'))
-        self.dirCtl = wxTextCtrl(panel, -1, '')
+        gridSizer.Add(wx.wxStaticText(panel, -1, 'directory to build:'))
+        self.dirCtl = wx.wxTextCtrl(panel, -1, '')
 
-        b = wxBoxSizer(wxHORIZONTAL)
-        b.Add(self.dirCtl, 1, wxEXPAND)
+        b = wx.wxBoxSizer(wx.wxHORIZONTAL)
+        b.Add(self.dirCtl, 1, wx.wxEXPAND)
 #        b.Add(10, 10, 0, wxEXPAND)
-        button = wxButton(panel, -1, 'select')
-        b.Add(button, 0, wxEXPAND)
-        EVT_BUTTON(frame, button.GetId(), self.select)
+        button = wx.wxButton(panel, -1, 'select')
+        b.Add(button, 0, wx.wxEXPAND)
+        wx.EVT_BUTTON(frame, button.GetId(), self.select)
 
-        gridSizer.Add(b, 0, wxEXPAND)
+        gridSizer.Add(b, 0, wx.wxEXPAND)
 
-        gridSizer.Add(wxStaticText(panel, -1, 'announce url:'))
-        self.annCtl = wxTextCtrl(panel, -1, 'http://my.tracker:6969/announce')
-        gridSizer.Add(self.annCtl, 0, wxEXPAND)
+        gridSizer.Add(wx.wxStaticText(panel, -1, 'announce url:'))
+        self.annCtl = wx.wxTextCtrl(panel, -1,
+                                    'http://my.tracker:6969/announce')
+        gridSizer.Add(self.annCtl, 0, wx.wxEXPAND)
 
-        gridSizer.Add(wxStaticText(panel, -1, 'piece size:'))
-        self.piece_length = wxChoice(
+        gridSizer.Add(wx.wxStaticText(panel, -1, 'piece size:'))
+        self.piece_length = wx.wxChoice(
             panel, -1, choices=['2 ** 21', '2 ** 20', '2 ** 19', '2 ** 18',
                                 '2 ** 17', '2 ** 16', '2 ** 15'])
         self.piece_length.SetSelection(3)
@@ -64,26 +65,27 @@ class DownloadInfo:
 
         gridSizer.AddGrowableCol(1)
 
-        border = wxBoxSizer(wxVERTICAL)
-        border.Add(gridSizer, 0, wxEXPAND | wxNORTH | wxEAST | wxWEST, 25)
-        b2 = wxButton(panel, -1, 'make')
+        border = wx.wxBoxSizer(wx.wxVERTICAL)
+        border.Add(gridSizer, 0,
+                   wx.wxEXPAND | wx.wxNORTH | wx.wxEAST | wx.wxWEST, 25)
+        b2 = wx.wxButton(panel, -1, 'make')
 #        border.Add(10, 10, 1, wxEXPAND)
-        border.Add(b2, 0, wxALIGN_CENTER | wxSOUTH, 20)
-        EVT_BUTTON(frame, b2.GetId(), self.complete)
+        border.Add(b2, 0, wx.wxALIGN_CENTER | wx.wxSOUTH, 20)
+        wx.EVT_BUTTON(frame, b2.GetId(), self.complete)
         panel.SetSizer(border)
         panel.SetAutoLayout(True)
 
     def select(self, x):
-        dl = wxDirDialog(self.frame,
-                         style=(wxDD_DEFAULT_STYLE | wxDD_NEW_DIR_BUTTON))
-        if dl.ShowModal() == wxID_OK:
+        dl = wx.wxDirDialog(
+            self.frame, style=(wx.wxDD_DEFAULT_STYLE | wx.wxDD_NEW_DIR_BUTTON))
+        if dl.ShowModal() == wx.wxID_OK:
             self.dirCtl.SetValue(dl.GetPath())
 
     def complete(self, x):
         if self.dirCtl.GetValue() == '':
-            dlg = wxMessageDialog(
+            dlg = wx.wxMessageDialog(
                 self.frame, message='You must select a directory',
-                caption='Error', style=(wxOK | wxICON_ERROR))
+                caption='Error', style=(wx.wxOK | wx.wxICON_ERROR))
             dlg.ShowModal()
             dlg.Destroy()
             return
@@ -102,32 +104,32 @@ class CompleteDir:
         self.a = a
         self.pl = pl
         self.flag = threading.Event()
-        frame = wxFrame(None, -1, 'BitTorrent make directory',
-                        size=wxSize(550, 250))
+        frame = wx.wxFrame(None, -1, 'BitTorrent make directory',
+                           size=wx.wxSize(550, 250))
         self.frame = frame
 
-        panel = wxPanel(frame, -1)
+        panel = wx.wxPanel(frame, -1)
 
-        gridSizer = wxFlexGridSizer(cols=1, vgap=15, hgap=8)
+        gridSizer = wx.wxFlexGridSizer(cols=1, vgap=15, hgap=8)
 
-        self.currentLabel = wxStaticText(panel, -1, 'checking file sizes')
-        gridSizer.Add(self.currentLabel, 0, wxEXPAND)
-        self.gauge = wxGauge(panel, -1, range=1000, style=wxGA_SMOOTH)
-        gridSizer.Add(self.gauge, 0, wxEXPAND)
-        gridSizer.Add(10, 10, 1, wxEXPAND)
-        self.button = wxButton(panel, -1, 'cancel')
-        gridSizer.Add(self.button, 0, wxALIGN_CENTER)
+        self.currentLabel = wx.wxStaticText(panel, -1, 'checking file sizes')
+        gridSizer.Add(self.currentLabel, 0, wx.wxEXPAND)
+        self.gauge = wx.wxGauge(panel, -1, range=1000, style=wx.wxGA_SMOOTH)
+        gridSizer.Add(self.gauge, 0, wx.wxEXPAND)
+        gridSizer.Add(10, 10, 1, wx.wxEXPAND)
+        self.button = wx.wxButton(panel, -1, 'cancel')
+        gridSizer.Add(self.button, 0, wx.wxALIGN_CENTER)
         gridSizer.AddGrowableRow(2)
         gridSizer.AddGrowableCol(0)
 
-        g2 = wxFlexGridSizer(cols=1, vgap=15, hgap=8)
-        g2.Add(gridSizer, 1, wxEXPAND | wxALL, 25)
+        g2 = wx.wxFlexGridSizer(cols=1, vgap=15, hgap=8)
+        g2.Add(gridSizer, 1, wx.wxEXPAND | wx.wxALL, 25)
         g2.AddGrowableRow(0)
         g2.AddGrowableCol(0)
         panel.SetSizer(g2)
         panel.SetAutoLayout(True)
-        EVT_BUTTON(frame, self.button.GetId(), self.done)
-        EVT_CLOSE(frame, self.done)
+        wx.EVT_BUTTON(frame, self.button.GetId(), self.done)
+        wx.EVT_CLOSE(frame, self.done)
         EVT_INVOKE(frame, self.onInvoke)
         frame.Show(True)
         threading.Thread(target=self.complete).start()
@@ -144,8 +146,9 @@ class CompleteDir:
         except (OSError, IOError) as e:
             self.currentLabel.SetLabel('Error!')
             self.button.SetLabel('Close')
-            dlg = wxMessageDialog(self.frame, message='Error - ' + str(e),
-                                  caption='Error', style=(wxOK | wxICON_ERROR))
+            dlg = wx.wxMessageDialog(
+                self.frame, message='Error - ' + str(e), caption='Error',
+                style=(wx.wxOK | wx.wxICON_ERROR))
             dlg.ShowModal()
             dlg.Destroy()
 
@@ -168,14 +171,14 @@ class CompleteDir:
 
     def invokeLater(self, func, args=[], kwargs={}):
         if not self.flag.isSet():
-            wxPostEvent(self.frame, InvokeEvent(func, args, kwargs))
+            wx.wxPostEvent(self.frame, InvokeEvent(func, args, kwargs))
 
     def done(self, event):
         self.flag.set()
         self.frame.Destroy()
 
 
-class btWxApp(wxApp):
+class btWxApp(wx.wxApp):
     def OnInit(self):
         d = DownloadInfo()
         d.frame.Show(True)

da183e807bd302fa15f149cbda31e310ef1c2c73
Chris Johnson <effigies@gmail.com>
2013-05-06 20:03:11 -0400

    DOC/STY pylint cleanup, including many new docstrings

--- BitTornado/NetworkAddress.py
+++ BitTornado/NetworkAddress.py
@@ -29,6 +29,8 @@ class Address(long):
     library.
 
     Subclass with number of bits and address family."""
+    bits = None
+    af = None
 
     def __str__(self):
         """Use socket library formatting"""
@@ -53,22 +55,26 @@ class Address(long):
         try:
             socket.inet_pton(cls.af, address)
             return True
-        except:
+        except socket.error:
             return False
 
-    def mask(self, n):
+    def mask(self, nbits):
         """Return an address with the first n bits preserved and the
         rest zeroes out."""
         ones = (1 << self.bits) - 1
-        return self.__class__(self & (ones << (self.bits - n)))
+        return self.__class__(self & (ones << (self.bits - nbits)))
 
 
 class IPv4(Address):
+    """Integer representation of IPv4 network addresses, building on the
+    socket library."""
     bits = 32
     af = socket.AF_INET
 
 
 class IPv6(Address):
+    """Integer representation of IPv6 network addresses, building on the
+    socket library."""
     bits = 128
     af = socket.AF_INET6
 
@@ -76,15 +82,20 @@ ADDRESSTYPES = (IPv4, IPv6)
 
 
 def addressToLong(address):
+    """Generic function to translate any recognized addresses to their
+    corresponding Address subclass instances."""
     for addrType in ADDRESSTYPES:
         if addrType.isString(address):
             return addrType.fromString(address)
-    else:
-        return None
+    raise ValueError('Invalid address: {}'.format(address))
 
 
-class AddressRange(object):
+class AddressRange(object):     # pylint: disable=R0903
+    """Range within a given address family that allows unions, comparisons,
+    and checks for inclusion."""
     def __init__(self, start, end=None):
+        """Create range of from start to end, or lift address into a
+        range, if no end."""
         if end is None:
             end = start
         assert start <= end
@@ -95,45 +106,46 @@ class AddressRange(object):
     def __str__(self):
         return '{}-{}'.format(self.start, self.end)
 
-    def __contains__(self, x):
-        if isinstance(x, AddressRange):
-            return x.start >= self.start and x.end <= self.end
-        return x >= self.start and x <= self.end
+    def __contains__(self, addr):
+        if isinstance(addr, AddressRange):
+            return addr.start >= self.start and addr.end <= self.end
+        return addr >= self.start and addr <= self.end
 
-    def __add__(self, x):
-        if isinstance(x, AddressRange):
-            if x.start > self.end + 1:
-                return (self, x)
-            elif self.start > x.end + 1:
-                return (x, self)
+    def __add__(self, addr):
+        if isinstance(addr, AddressRange):
+            if addr.start > self.end + 1:
+                return (self, addr)
+            elif self.start > addr.end + 1:
+                return (addr, self)
             else:
-                return AddressRange(min(self.start, x.start),
-                                    max(self.end, x.end))
-        if x > self.end + 1:
-            return (self, AddressRange(x))
-        elif self.start > x + 1:
-            return (AddressRange(x), self)
+                return AddressRange(min(self.start, addr.start),
+                                    max(self.end, addr.end))
+        if addr > self.end + 1:
+            return (self, AddressRange(addr))
+        elif self.start > addr + 1:
+            return (AddressRange(addr), self)
         else:
-            return AddressRange(min(self.start, x), max(self.end, x))
+            return AddressRange(min(self.start, addr), max(self.end, addr))
 
-    def __lt__(self, x):
+    def __lt__(self, addr):
         """True if there is at least one address above the range and below x"""
-        if isinstance(x, AddressRange):
-            return x.start > self.end + 1
-        return x > self.end + 1
+        if isinstance(addr, AddressRange):
+            return addr.start > self.end + 1
+        return addr > self.end + 1
 
-    def __gt__(self, x):
+    def __gt__(self, addr):
         """True if there is at least one address below the range and above x"""
-        if isinstance(x, AddressRange):
-            return self.start > x.end + 1
-        return self.start > x + 1
+        if isinstance(addr, AddressRange):
+            return self.start > addr.end + 1
+        return self.start > addr + 1
 
-    def __eq__(self, x):
-        return self.start == x.start and self.end == x.end
+    def __eq__(self, addr):
+        return self.start == addr.start and self.end == addr.end
 
     @classmethod
     def fromString(cls, iprange):
-        start, dash, end = iprange.partition('-')
+        """Parse address range of the form start-end"""
+        start, _dash, end = iprange.partition('-')
         startip = addressToLong(start)
         if end:
             endip = addressToLong(end)
@@ -144,74 +156,86 @@ class AddressRange(object):
 
 
 class Subnet(AddressRange):
+    """Address range that operates on the logic of CIDR blocks.
+    
+    If addition of new addresses breaks this logic, revert to AddressRange."""
     def __init__(self, address, cidr):
         self.address = address.mask(cidr)
         self.cidr = cidr
 
-        self.start = self.address
+        start = self.address
         diff = (1 << (address.bits - cidr)) - 1
-        self.end = address.__class__(self.start + diff)
+        end = address.__class__(start + diff)
 
-        self.family = type(address)
+        super(Subnet, self).__init__(start, end)
 
     def __str__(self):
         return '{}/{:d}'.format(self.address, self.cidr)
 
-    def __contains__(self, x):
+    def __contains__(self, addr):
         """Determine if an address or Subnet is subsumed by this Subset"""
-        if isinstance(x, Subnet):
-            return x.cidr > self.cidr and x.address in self
-        return super(Subnet, self).__contains__(x)
+        if isinstance(addr, Subnet):
+            return addr.cidr > self.cidr and addr.address in self
+        return super(Subnet, self).__contains__(addr)
 
-    def __add__(self, x):
+    def __add__(self, addr):
         """If a Subnet subsumes another range, keep the Subnet apparatus.
         If not, revert to AddressRange addition."""
-        if x in self:
+        if addr in self:
             return self
-        elif self in x:
-            return x
+        elif self in addr:
+            return addr
         else:
-            return super(Subnet, self).__add__(x)
+            return super(Subnet, self).__add__(addr)
 
     @classmethod
     def fromCIDR(cls, netstring):
-        ipstring, slash, cidrstring = netstring.partition('/')
-        ip = addressToLong(ipstring)
+        """Parse CIDR string of the form IP/CIDR"""
+        ipstring, _slash, cidrstring = netstring.partition('/')
+        addr = addressToLong(ipstring)
         if cidrstring:
             cidr = int(cidrstring)
         else:
-            cidr = ip.bits
-        return cls(ip, cidr)
+            cidr = addr.bits
+        return cls(addr, cidr)
 
 
 class AddrList(object):
+    """Collection of addresses with no constraints on contiguity,
+    featuring insertion functions and inclusion tests."""
     def __init__(self):
         self.ranges = {IPv4: [],
                        IPv6: []}
 
-    def addIP(self, ip):
-        self.addAddressRange(AddressRange(addressToLong(ip)))
+    def addIP(self, addr):
+        """Insert individual address string into list"""
+        self.addAddressRange(AddressRange(addressToLong(addr)))
 
     def addSubnet(self, subnet):
+        """Insert CIDR block string into list"""
         self.addAddressRange(Subnet.fromCIDR(subnet))
 
     def addRange(self, iprange):
+        """Insert contiguous address range string into list"""
         self.addAddressRange(AddressRange.fromString(iprange))
 
     def addAddressRange(self, iprange):
+        """Insert AddressRange into list, combining overlapping list
+        elements into a minimal set of ranges."""
         ranges = self.ranges[iprange.family]
 
-        l = bisect.bisect_left(ranges, iprange)
-        r = bisect.bisect_right(ranges, iprange)
+        left = bisect.bisect_left(ranges, iprange)
+        right = bisect.bisect_right(ranges, iprange)
 
-        newseg = reduce(operator.add, ranges[l:r], iprange)
-        ranges[l:r] = [newseg]
+        newseg = reduce(operator.add, ranges[left:right], iprange)
+        ranges[left:right] = [newseg]
 
     def __contains__(self, address):
-        ip = addressToLong(address)
-        return any(ip in r for r in self.ranges[type(ip)])
+        longip = addressToLong(address)
+        return any(longip in r for r in self.ranges[type(longip)])
 
     def set_intranet_addresses(self):
+        """Add addresses corresponding to reserved instranet blocks"""
         self.addSubnet('127.0.0.1/8')
         self.addSubnet('10.0.0.0/8')
         self.addSubnet('172.16.0.0/12')
@@ -230,48 +254,51 @@ class AddrList(object):
 
         Leading whitespace is ignored, as are lines beginning with '#'
         """
-        with open(filename, 'r') as f:
-            for line in f:
+        with open(filename, 'r') as fieldlistfile:
+            for line in fieldlistfile:
                 fields = line.split()
                 if not fields or fields[0][0] == '#':
                     continue
 
                 try:
                     self.addSubnet(fields[0])
-                except:
+                except ValueError:
                     print '*** WARNING *** could not parse IP range: ' + line
 
     def read_rangelist(self, filename):
         """Read a list from a file in the format 'whatever:whatever:ip[-ip]
         (not IPv6 compatible at all)"""
-        with open(filename, 'r') as f:
-            for line in f:
+        with open(filename, 'r') as rangelistfile:
+            for line in rangelistfile:
                 fields = line.split()
                 if not fields or fields[0][0] == '#':
                     continue
 
                 try:
                     self.addRange(fields[0].split(':')[-1])
-                except:
+                except ValueError:
                     print '*** WARNING *** could not parse IP range: ' + line
 
-ipv4addrmask = IPv6.fromString('::ffff:0:0')
+IPV4ADDRMASK = IPv6.fromString('::ffff:0:0')
 
 
-def ipv6_to_ipv4(ip):
-    unmasked = IPv4(IPv6.fromString(ip) ^ ipv4addrmask)
+def ipv6_to_ipv4(ip6):
+    """Convert IPv6 address to IPv4 address, if possible"""
+    unmasked = IPv4(IPv6.fromString(ip6) ^ IPV4ADDRMASK)
     if unmasked >> IPv4.bits:
         raise ValueError("not convertible to IPv4")
     return str(unmasked)
 
-is_ipv4 = IPv4.isString
+is_ipv4 = IPv4.isString     # pylint: disable=C0103
 
 
-def to_ipv4(ip):
-    if is_ipv4(ip):
-        return ip
-    return ipv6_to_ipv4(ip)
+def to_ipv4(addr):
+    """Convert IP string to IPv4 string"""
+    if is_ipv4(addr):
+        return addr
+    return ipv6_to_ipv4(addr)
 
 
-def is_valid_ip(ip):
-    return (IPv4.isString(ip) or IPv6.isString(ip))
+def is_valid_ip(addr):
+    """Test if string is valid IPv4 or IPv6"""
+    return (IPv4.isString(addr) or IPv6.isString(addr))

effigies/BitTornado

commit 1eacc1d40a6caa6fc2b0102bf58b97eb273ce4d2

Reduce namespace pollution, eliminate join(join(a,b),c) pattern

btlaunchmanycurses.py
@@ -14,13 +14,14 @@ if PSYCO.psyco:
     except:
         pass
 
+import sys
+import os
+import time
+import signal
+import threading
 from BitTornado.launchmanycore import LaunchMany
 from BitTornado.download_bt1 import defaults, get_usage
 from BitTornado.parseargs import parseargs
-from threading import Event
-from sys import argv, exit
-from time import time, localtime, strftime
-import sys, os
 from BitTornado import version, report_email
 from BitTornado.ConfigDir import ConfigDir
 
@@ -28,7 +29,6 @@ try:
     import curses
     import curses.panel
     from curses.wrapper import wrapper as curses_wrapper
-    from signal import signal, SIGWINCH 
 except:
     print 'Textmode GUI initialization failed, cannot proceed.'
     print
@@ -94,8 +94,8 @@ class CursesDisplayer:
         self.scroll_time = 0
         
         self.scrwin = scrwin
-        signal(SIGWINCH, self.winch_handler)
-        self.changeflag = Event()
+        signal.signal(signal.SIGWINCH, self.winch_handler)
+        self.changeflag = threading.Event()
         self._remake_window()
 
     def winch_handler(self, signum, stackframe):
@@ -174,8 +174,8 @@ class CursesDisplayer:
         if 3*len(data) <= self.mainwinh:
             self.scroll_pos = 0
             self.scrolling = False
-        elif self.scroll_time + DOWNLOAD_SCROLL_RATE < time():
-            self.scroll_time = time()
+        elif self.scroll_time + DOWNLOAD_SCROLL_RATE < time.time():
+            self.scroll_time = time.time()
             self.scroll_pos += 1
             self.scrolling = True
             if self.scroll_pos >= 3*len(data)+2:
@@ -195,7 +195,7 @@ class CursesDisplayer:
                     break
             ( name, status, progress, peers, seeds, seedsmsg, dist,
               uprate, dnrate, upamt, dnamt, size, t, msg ) = data[ii]
-            t = fmttime(t)
+            t = time.fmttime(t)
             if t:
                 status = t
             name = ljust(name,self.mainwinw-32)
@@ -254,7 +254,7 @@ class CursesDisplayer:
         return inchar in (ord('q'),ord('Q'))
 
     def message(self, s):
-        self.messages.append(strftime('%x %X - ',localtime(time()))+s)
+        self.messages.append(time.strftime('%x %X - ',time.localtime())+s)
         self._display_messages()
 
     def _display_messages(self):
@@ -277,9 +277,9 @@ def LaunchManyWrapper(scrwin, config):
 
 
 if __name__ == '__main__':
-    if argv[1:] == ['--version']:
+    if sys.argv[1:] == ['--version']:
         print version
-        exit(0)
+        sys.exit(0)
     defaults.extend( [
         ( 'parse_dir_interval', 60,
           "how often to rescan the torrent directory, in seconds" ),
@@ -297,12 +297,12 @@ if __name__ == '__main__':
         defaults.append(('save_options',0,
          "whether to save the current options as the new default configuration " +
          "(only for btlaunchmanycurses.py)"))
-        if len(argv) < 2:
+        if len(sys.argv) < 2:
             print "Usage: btlaunchmanycurses.py <directory> <global options>\n"
             print "<directory> - directory to look for .torrent files (semi-recursive)"
             print get_usage(defaults, 80, configdefaults)
-            exit(1)
-        config, args = parseargs(argv[1:], defaults, 1, 1, configdefaults)
+            sys.exit(1)
+        config, args = parseargs(sys.argv[1:], defaults, 1, 1, configdefaults)
         if config['save_options']:
             configdir.saveConfig(config)
         configdir.deleteOldCacheData(config['expire_cache_data'])
@@ -311,7 +311,7 @@ if __name__ == '__main__':
         config['torrent_dir'] = args[0]
     except ValueError, e:
         print 'error: ' + str(e) + '\nrun with no args for parameter explanations'
-        exit(1)
+        sys.exit(1)
 
     curses_wrapper(LaunchManyWrapper, config)
     if Exceptions:

bt-t-make.py
@@ -13,15 +13,15 @@ if PSYCO.psyco:
     except:
         pass
 
-from sys import argv, platform, version
-assert version >= '2', "Install Python 2.0 or greater"
+import sys
+import os
+import shutil
+import threading
+import traceback
 from BitTornado.BT1.makemetafile import make_meta_file
-from threading import Event, Thread, Lock
 from BitTornado.bencode import bencode,bdecode
-import sys, os, shutil
-from os import getcwd, listdir
-from os.path import join, isdir
-from traceback import print_exc
+
+assert sys.version >= '2', "Install Python 2.0 or greater"
 try:
     from wxPython.wx import *
 except:
@@ -36,7 +36,7 @@ except:
 
 basepath=os.path.abspath(os.path.dirname(sys.argv[0]))
 
-if platform == 'win32':
+if sys.platform == 'win32':
     DROP_HERE = '(drop here)'
 else:
     DROP_HERE = ''
@@ -61,9 +61,9 @@ class BasicDownloadInfo:
         self.config = config
         self.calls = calls
         
-        self.uiflag = Event()
-        self.cancelflag = Event()
-        self.switchlock = Lock()
+        self.uiflag = threading.Event()
+        self.cancelflag = threading.Event()
+        self.switchlock = threading.Lock()
         self.working = False
         self.queue = []
         wxInitAllImageHandlers()
@@ -138,7 +138,7 @@ class BasicDownloadInfo:
         self.refresh_thostlist()
         self._set_thost()
 
-        if platform == 'win32':
+        if sys.platform == 'win32':
             self.dropTargetPtr.DragAcceptFiles(True)
             EVT_DROP_FILES(self.dropTargetPtr, self.selectdrop)
  
@@ -212,7 +212,7 @@ class BasicDownloadInfo:
         if self.working:
             self.working = False
             self.cancelflag.set()
-            self.cancelflag = Event()
+            self.cancelflag = threading.Event()
             self.queue = []
             self.statustext.SetLabel('CANCELED')
             self.calls['dropTargetError']()
@@ -227,7 +227,7 @@ class BasicDownloadInfo:
     def refresh_thostlist(self):
         l = []
         d = 0
-        for f in listdir(join(basepath,'thosts')):
+        for f in os.listdir(os.path.join(basepath,'thosts')):
             if f[-6:].lower() == '.thost':
                 l.append(f)
                 if f == self.thostselection:
@@ -257,7 +257,7 @@ class BasicDownloadInfo:
                 self.thostselection = self.thostlist[n-1]
 
     def _set_thost(self):
-        self._announcecopy(join(join(basepath,'thosts'),self.thostselection))
+        self._announcecopy(os.path.join(basepath,'thosts',self.thostselection))
         self.calls['setCurrentTHost'](self.thostselection)
 
     def onInvoke(self, event):
@@ -314,9 +314,9 @@ class AdvancedDownloadInfo:
         self.config = config
         self.calls = calls
         
-        self.uiflag = Event()
-        self.cancelflag = Event()
-        self.switchlock = Lock()
+        self.uiflag = threading.Event()
+        self.cancelflag = threading.Event()
+        self.switchlock = threading.Lock()
         self.working = False
         self.queue = []
         wxInitAllImageHandlers()
@@ -487,7 +487,7 @@ class AdvancedDownloadInfo:
         self.refresh_thostlist()
         self._set_thost()
 
-        if platform == 'win32':
+        if sys.platform == 'win32':
             self.dropTargetPtr.DragAcceptFiles(True)
             EVT_DROP_FILES(self.dropTargetPtr, self.selectdrop)
             self.groupSizer1Box.DragAcceptFiles(True)
@@ -661,15 +661,15 @@ class AdvancedDownloadInfo:
         if self.working:
             self.working = False
             self.cancelflag.set()
-            self.cancelflag = Event()
+            self.cancelflag = threading.Event()
             self.queue = []
             self.statustext.SetLabel('CANCELED')
             self.calls['dropTargetError']()
         self.switchlock.release()
 
     def selectDropTarget(self, x):
-        dl = wxFileDialog (self.frame, 'Choose image to use', join(basepath,'targets'),
-                        join(join(basepath,'targets'), self.config['target']),
+        dl = wxFileDialog (self.frame, 'Choose image to use', os.path.join(basepath,'targets'),
+                        os.path.join(basepath,'targets', self.config['target']),
                         'Supported images (*.bmp,*.gif)|*.*', wxOPEN|wxHIDE_READONLY)
         if dl.ShowModal() == wxID_OK:
             try:
@@ -687,7 +687,7 @@ class AdvancedDownloadInfo:
     def refresh_thostlist(self):
         l = []
         d = 0
-        for f in listdir(join(basepath,'thosts')):
+        for f in os.listdir(os.path.join(basepath,'thosts')):
             if f[-6:].lower() == '.thost':
                 l.append(f)
                 if f == self.thostselection:
@@ -734,7 +734,7 @@ class AdvancedDownloadInfo:
                 self._set_thost()
 
     def _set_thost(self):
-        self._announcecopy(join(join(basepath,'thosts'),self.thostselection))
+        self._announcecopy(os.path.join(basepath,'thosts',self.thostselection))
         self.calls['setCurrentTHost'](self.thostselection)
 
     def set_default_thost(self, x):
@@ -786,7 +786,7 @@ class AdvancedDownloadInfo:
         else:
             d = '.thost'
         dl = wxFileDialog (self.frame, 'Save tracker data as',
-                           join(basepath,'thosts'), d, '*.thost',
+                           os.path.join(basepath,'thosts'), d, '*.thost',
                            wxSAVE|wxOVERWRITE_PROMPT)
         if dl.ShowModal() != wxID_OK:
             return
@@ -809,7 +809,7 @@ class AdvancedDownloadInfo:
             dlg.Destroy()
             return
         dlg.Destroy()
-        os.remove(join(join(basepath,'thosts'),self.thostselection))
+        os.remove(os.path.join(basepath,'thosts',self.thostselection))
         self.thostselection = None
         self.refresh_thostlist()
 
@@ -871,7 +871,7 @@ class MakeMetafile:
         self.call = external
 #        self.uiflag = external.uiflag
         self.uiflag = external.cancelflag
-        Thread(target = self.complete).start()
+        threading.Thread(target = self.complete).start()
 
     def complete(self):
         try:
@@ -882,7 +882,7 @@ class MakeMetafile:
         except (OSError, IOError), e:
             self.failed(e)
         except Exception, e:
-            print_exc()
+            traceback.print_exc()
             self.failed(e)
 
     def failed(self, e):
@@ -994,16 +994,16 @@ class T_make:
 
     def _dropTargetRead(self, new):
         a,b = os.path.split(new)
-        if a and a != join(basepath,'targets'):
-            if a != join(basepath,'targets'):
+        if a and a != os.path.join(basepath,'targets'):
+            if a != os.path.join(basepath,'targets'):
                 b1,b2 = os.path.splitext(b)
                 z = 0
-                while os.path.isfile(join(join(basepath,'targets'),b)):
+                while os.path.isfile(os.path.join(basepath,'targets',b)):
                     z += 1
                     b = b1+'('+str(z)+')'+b2
-                shutil.copyfile(newname,join(join(basepath,'targets'),b))
+                shutil.copyfile(newname,os.path.join(basepath,'targets',b))
             new = b
-        name = join(join(basepath,'targets'),new)
+        name = os.path.join(basepath,'targets',new)
         garbage, e = os.path.splitext(new.lower())
         if e == '.gif':
             bmp = wxBitmap(name, wxBITMAP_TYPE_GIF)

btcompletedirgui.py
@@ -12,13 +12,11 @@ if PSYCO.psyco:
     except:
         pass
 
-from sys import argv, version
-assert version >= '2', "Install Python 2.0 or greater"
-from BitTornado.BT1.makemetafile import completedir
-from threading import Event, Thread
 import sys
-from os import getcwd
-from os.path import join
+import os
+import threading
+from BitTornado.BT1.makemetafile import completedir
+assert sys.version >= '2', "Install Python 2.0 or greater"
 try:
     from wxPython.wx import *
 except:
@@ -111,7 +109,7 @@ class CompleteDir:
         self.d = d
         self.a = a
         self.pl = pl
-        self.flag = Event()
+        self.flag = threading.Event()
         frame = wxFrame(None, -1, 'BitTorrent make directory', size = wxSize(550, 250))
         self.frame = frame
 
@@ -139,7 +137,7 @@ class CompleteDir:
         EVT_CLOSE(frame, self.done)
         EVT_INVOKE(frame, self.onInvoke)
         frame.Show(True)
-        Thread(target = self.complete).start()
+        threading.Thread(target = self.complete).start()
 
     def complete(self):
         params = {'piece_size_pow2': self.pl}
@@ -167,7 +165,7 @@ class CompleteDir:
         self.invokeLater(self.onfile, [f])
 
     def onfile(self, f):
-        self.currentLabel.SetLabel('building ' + join(self.d, f) + '.torrent')
+        self.currentLabel.SetLabel('building ' + os.path.join(self.d, f) + '.torrent')
 
     def onInvoke(self, event):
         if not self.flag.isSet():

btdownloadcurses.py
@@ -14,19 +14,18 @@ if PSYCO.psyco:
     except:
         pass
 
+import sys
+import os
+import sha
+import time
+import signal
+import random
+import socket
+import threading
 from BitTornado.download_bt1 import BT1Download, defaults, parse_params, get_usage, get_response
 from BitTornado.RawServer import RawServer, UPnP_ERROR
-from random import seed
-from socket import error as socketerror
 from BitTornado.bencode import bencode
 from BitTornado.natpunch import UPnP_test
-from threading import Event
-from os.path import abspath
-from signal import signal, SIGWINCH
-from sha import sha
-from sys import argv, exit
-import sys
-from time import time, strftime
 from BitTornado.clock import clock
 from BitTornado import createPeerID, version
 from BitTornado.ConfigDir import ConfigDir
@@ -35,7 +34,6 @@ try:
     import curses
     import curses.panel
     from curses.wrapper import wrapper as curses_wrapper
-    from signal import signal, SIGWINCH 
 except:
     print 'Textmode GUI initialization failed, cannot proceed.'
     print
@@ -89,8 +87,8 @@ class CursesDisplayer:
         self.errlist = errlist
         self.doneflag = doneflag
         
-        signal(SIGWINCH, self.winch_handler)
-        self.changeflag = Event()
+        signal.signal(signal.SIGWINCH, self.winch_handler)
+        self.changeflag = threading.Event()
 
         self.done = 0
         self.file = ''
@@ -169,13 +167,13 @@ class CursesDisplayer:
         self.display()
 
     def error(self, errormsg):
-        newerrmsg = strftime('[%H:%M:%S] ') + errormsg
+        newerrmsg = time.strftime('[%H:%M:%S] ') + errormsg
         self.errors.append(newerrmsg)
         self.errlist.append(newerrmsg)
         self.display()
 
-    def display(self, dpflag = Event(), fractionDone = None, timeEst = None,
-            downRate = None, upRate = None, activity = None,
+    def display(self, dpflag = threading.Event(), fractionDone = None,
+            timeEst = None, downRate = None, upRate = None, activity = None,
             statistics = None, spew = None, **kws):
 
         inchar = self.fieldwin.getch()
@@ -300,11 +298,11 @@ class CursesDisplayer:
         self.fileSize = fmtsize(size)
         if saveas == '':
             saveas = default
-        self.downloadTo = abspath(saveas)
+        self.downloadTo = os.path.abspath(saveas)
         return saveas
 
 def run(scrwin, errlist, params):
-    doneflag = Event()
+    doneflag = threading.Event()
     d = CursesDisplayer(scrwin, errlist, doneflag)
     try:
         while 1:
@@ -328,7 +326,7 @@ def run(scrwin, errlist, params):
             configdir.deleteOldCacheData(config['expire_cache_data'])
 
             myid = createPeerID()
-            seed(myid)
+            random.seed(myid)
 
             rawserver = RawServer(doneflag, config['timeout_check_interval'],
                                   config['timeout'], ipv6_enable = config['ipv6_enabled'],
@@ -341,7 +339,7 @@ def run(scrwin, errlist, params):
                                     config['bind'], ipv6_socket_style = config['ipv6_binds_v4'],
                                     upnp = upnp_type, randomizer = config['random_port'])
                     break
-                except socketerror, e:
+                except socket.error, e:
                     if upnp_type and e == UPnP_ERROR:
                         d.error('WARNING: COULD NOT FORWARD VIA UPnP')
                         upnp_type = 0
@@ -354,7 +352,7 @@ def run(scrwin, errlist, params):
             if not response:
                 break
 
-            infohash = sha(bencode(response['info'])).digest()
+            infohash = sha.sha(bencode(response['info'])).digest()
             
             dow = BT1Download(d.display, d.finished, d.error, d.error, doneflag,
                             config, response, infohash, myid, rawserver, listen_port,
@@ -390,16 +388,16 @@ def run(scrwin, errlist, params):
 
 
 if __name__ == '__main__':
-    if argv[1:] == ['--version']:
+    if sys.argv[1:] == ['--version']:
         print version
-        exit(0)
-    if len(argv) <= 1:
+        sys.exit(0)
+    if len(sys.argv) <= 1:
         print "Usage: btdownloadcurses.py <global options>\n"
         print get_usage(defaults)
-        exit(1)
+        sys.exit(1)
 
     errlist = []
-    curses_wrapper(run, errlist, argv[1:])
+    curses_wrapper(run, errlist, sys.argv[1:])
 
     if errlist:
        print "These errors occurred during execution:"

btdownloadgui.py
@@ -13,34 +13,33 @@ if PSYCO.psyco:
     except:
         pass
 
-from sys import argv, version, exit
-assert version >= '2', "Install Python 2.0 or greater"
+import sys
+import os
+import re
+import sha
+import random
+import socket
+import traceback
+from webbrowser import open_new
+from threading import Event, Thread
+from time import strftime, localtime, sleep
+from StringIO import StringIO
+from BitTornado.download_bt1 import BT1Download, defaults, parse_params, get_usage, get_response
+from BitTornado.RawServer import RawServer, UPnP_ERROR
+from BitTornado.ConnChoice import *
+from BitTornado.ConfigReader import configReader
+from BitTornado.bencode import bencode, bdecode
+from BitTornado.natpunch import UPnP_test
+from BitTornado.clock import clock
+from BitTornado import version, createPeerID, report_email
+
+assert sys.version >= '2', "Install Python 2.0 or greater"
 
 try:
     from wxPython.wx import *
 except:
     print 'wxPython is either not installed or has not been installed properly.'
-    exit(1)
-from BitTornado.download_bt1 import BT1Download, defaults, parse_params, get_usage, get_response
-from BitTornado.RawServer import RawServer, UPnP_ERROR
-from random import seed
-from socket import error as socketerror
-from BitTornado.ConnChoice import *
-from BitTornado.ConfigReader import configReader
-from BitTornado.bencode import bencode, bdecode
-from BitTornado.natpunch import UPnP_test
-from threading import Event, Thread
-from os.path import *
-from os import getcwd
-from time import strftime, time, localtime, sleep
-from BitTornado.clock import clock
-from webbrowser import open_new
-from traceback import print_exc
-from StringIO import StringIO
-from sha import sha
-import re
-import sys, os
-from BitTornado import version, createPeerID, report_email
+    sys.exit(1)
 
 try:
     True
@@ -1990,12 +1989,12 @@ class DownloadInfoFrame:
                 start_dir = self.configfileargs['gui_default_savedir']
             else:
                 start_dir = self.configfileargs['last_saved']
-            if not isdir(start_dir):    # if it's not set properly
+            if not os.path.isdir(start_dir):    # if it's not set properly
                 start_dir = '/'    # yes, this hack does work in Windows
             if dir:
                 start_dir1 = start_dir
-                if isdir(join(start_dir,default)):
-                    start_dir = join(start_dir,default)
+                if os.path.isdir(os.path.join(start_dir,default)):
+                    start_dir = os.path.join(start_dir,default)
                 dl = wxDirDialog(self.frame,
                         'Choose a directory to save to, pick a partial download to resume',
                         defaultPath = start_dir, style = wxDD_DEFAULT_STYLE | wxDD_NEW_DIR_BUTTON)
@@ -2014,14 +2013,14 @@ class DownloadInfoFrame:
             if d == start_dir:
                 d = start_dir1
             bucket[0] = d
-            d1,d2 = split(d)
+            d1,d2 = os.path.split(d)
             if d2 == default:
                 d = d1
             self.configfile.WriteLastSaved(d)
 
         else:
             bucket[0] = saveas
-            default = basename(saveas)
+            default = os.path.basename(saveas)
 
         self.onChooseFileDone(default, size)
         f.set()
@@ -2031,7 +2030,7 @@ class DownloadInfoFrame:
 
     def onChooseFileDone(self, name, size):
         self.torrentsize = size
-        lname = basename(name)
+        lname = os.path.basename(name)
         self.filename = lname
         self.fileNameText.SetLabel('%s' % (lname))
         self.fileSizeText.SetLabel('(%.2f MiB)' % (float(size) / (1 << 20)))
@@ -2126,7 +2125,7 @@ class DownloadInfoFrame:
 
     def exception(self):
         data = StringIO()
-        print_exc(file = data)
+        traceback.print_exc(file = data)
         print data.getvalue()   # report exception here too
         self.on_errorwindow(data.getvalue())
 
@@ -2252,7 +2251,7 @@ def _next(params, d, doneflag, configfile):
                 break
 
             myid = createPeerID()
-            seed(myid)
+            random.seed(myid)
             
             rawserver = RawServer(doneflag, config['timeout_check_interval'],
                                   config['timeout'], ipv6_enable = config['ipv6_enabled'],
@@ -2265,7 +2264,7 @@ def _next(params, d, doneflag, configfile):
                                     config['bind'], ipv6_socket_style = config['ipv6_binds_v4'],
                                     upnp = upnp_type, randomizer = config['random_port'])
                     break
-                except socketerror, e:
+                except socket.error, e:
                     if upnp_type and e == UPnP_ERROR:
                         d.error('WARNING: COULD NOT FORWARD VIA UPnP')
                         upnp_type = 0
@@ -2279,7 +2278,7 @@ def _next(params, d, doneflag, configfile):
             if not response:
                 break
 
-            infohash = sha(bencode(response['info'])).digest()
+            infohash = sha.sha(bencode(response['info'])).digest()
             
             torrentdata = configfile.getTorrentData(infohash)
             if torrentdata:
@@ -2352,7 +2351,7 @@ def _next(params, d, doneflag, configfile):
     except:
         err = True
         data = StringIO()
-        print_exc(file = data)
+        traceback.print_exc(file = data)
         print data.getvalue()   # report exception here too
         d.errorwindow(data.getvalue())
     try:
@@ -2367,7 +2366,7 @@ def _next(params, d, doneflag, configfile):
 
 
 if __name__ == '__main__':
-    if argv[1:] == ['--version']:
+    if sys.argv[1:] == ['--version']:
         print version
-        exit(0)
-    run(argv[1:])
+        sys.exit(0)
+    run(sys.argv[1:])

btdownloadheadless.py
@@ -12,18 +12,17 @@ if PSYCO.psyco:
     except:
         pass
     
+import sys
+import os
+import sha
+import time
+import random
+import socket
+import threading
 from BitTornado.download_bt1 import BT1Download, defaults, parse_params, get_usage, get_response
 from BitTornado.RawServer import RawServer, UPnP_ERROR
-from random import seed
-from socket import error as socketerror
 from BitTornado.bencode import bencode
 from BitTornado.natpunch import UPnP_test
-from threading import Event
-from os.path import abspath
-from sys import argv, stdout
-import sys
-from sha import sha
-from time import strftime
 from BitTornado.clock import clock
 from BitTornado import createPeerID, version
 from BitTornado.ConfigDir import ConfigDir
@@ -85,8 +84,8 @@ class HeadlessDisplayer:
         self.errors.append(errormsg)
         self.display()
 
-    def display(self, dpflag = Event(), fractionDone = None, timeEst = None, 
-            downRate = None, upRate = None, activity = None,
+    def display(self, dpflag = threading.Event(), fractionDone = None,
+            timeEst = None, downRate = None, upRate = None, activity = None,
             statistics = None,  **kws):
         if self.last_update_time + 0.1 > clock() and fractionDone not in (0.0, 1.0) and activity is not None:
             return
@@ -123,14 +122,14 @@ class HeadlessDisplayer:
         print 'share rating:  ', self.shareRating
         print 'seed status:   ', self.seedStatus
         print 'peer status:   ', self.peerStatus
-        stdout.flush()
+        sys.stdout.flush()
         dpflag.set()        
 
     def chooseFile(self, default, size, saveas, dir):
         self.file = '%s (%.1f MB)' % (default, float(size) / (1 << 20))
         if saveas != '':
             default = saveas
-        self.downloadTo = abspath(default)
+        self.downloadTo = os.path.abspath(default)
         return default
 
     def newpath(self, path):
@@ -167,9 +166,9 @@ def run(params):
         configdir.deleteOldCacheData(config['expire_cache_data'])
 
         myid = createPeerID()
-        seed(myid)
+        random.seed(myid)
         
-        doneflag = Event()
+        doneflag = threading.Event()
         def disp_exception(text):
             print text
         rawserver = RawServer(doneflag, config['timeout_check_interval'],
@@ -182,7 +181,7 @@ def run(params):
                                 config['bind'], ipv6_socket_style = config['ipv6_binds_v4'],
                                 upnp = upnp_type, randomizer = config['random_port'])
                 break
-            except socketerror, e:
+            except socket.error, e:
                 if upnp_type and e == UPnP_ERROR:
                     print 'WARNING: COULD NOT FORWARD VIA UPnP'
                     upnp_type = 0
@@ -195,7 +194,7 @@ def run(params):
         if not response:
             break
 
-        infohash = sha(bencode(response['info'])).digest()
+        infohash = sha.sha(bencode(response['info'])).digest()
 
         dow = BT1Download(h.display, h.finished, h.error, disp_exception, doneflag,
                         config, response, infohash, myid, rawserver, listen_port,
@@ -226,19 +225,19 @@ def run(params):
         h.failed()
 
 if __name__ == '__main__':
-    if argv[1:] == ['--version']:
+    if sys.argv[1:] == ['--version']:
         print version
         sys.exit(0)
 
     if PROFILER:
         import profile, pstats
         p = profile.Profile()
-        p.runcall(run, argv[1:])
-        log = open('profile_data.'+strftime('%y%m%d%H%M%S')+'.txt','a')
+        p.runcall(run, sys.argv[1:])
+        log = open('profile_data.'+time.strftime('%y%m%d%H%M%S')+'.txt','a')
         normalstdout = sys.stdout
         sys.stdout = log
 #        pstats.Stats(p).strip_dirs().sort_stats('cumulative').print_stats()
         pstats.Stats(p).strip_dirs().sort_stats('time').print_stats()
         sys.stdout = normalstdout
     else:
-        run(argv[1:])
+        run(sys.argv[1:])

btlaunchmany.py
@@ -12,12 +12,11 @@ if PSYCO.psyco:
     except:
         pass
 
+import sys
+import os
 from BitTornado.launchmanycore import LaunchMany
 from BitTornado.download_bt1 import defaults, get_usage
 from BitTornado.parseargs import parseargs
-from threading import Event
-from sys import argv, exit
-import sys, os
 from BitTornado import version, report_email
 from BitTornado.ConfigDir import ConfigDir
 
@@ -68,9 +67,9 @@ class HeadlessDisplayer:
 
 
 if __name__ == '__main__':
-    if argv[1:] == ['--version']:
+    if sys.argv[1:] == ['--version']:
         print version
-        exit(0)
+        sys.exit(0)
     defaults.extend( [
         ( 'parse_dir_interval', 60,
           "how often to rescan the torrent directory, in seconds" ),
@@ -88,12 +87,12 @@ if __name__ == '__main__':
         defaults.append(('save_options',0,
          "whether to save the current options as the new default configuration " +
          "(only for btlaunchmany.py)"))
-        if len(argv) < 2:
+        if len(sys.argv) < 2:
             print "Usage: btlaunchmany.py <directory> <global options>\n"
             print "<directory> - directory to look for .torrent files (semi-recursive)"
             print get_usage(defaults, 80, configdefaults)
-            exit(1)
-        config, args = parseargs(argv[1:], defaults, 1, 1, configdefaults)
+            sys.exit(1)
+        config, args = parseargs(sys.argv[1:], defaults, 1, 1, configdefaults)
         if config['save_options']:
             configdir.saveConfig(config)
         configdir.deleteOldCacheData(config['expire_cache_data'])
@@ -102,7 +101,7 @@ if __name__ == '__main__':
         config['torrent_dir'] = args[0]
     except ValueError, e:
         print 'error: ' + str(e) + '\nrun with no args for parameter explanations'
-        exit(1)
+        sys.exit(1)
 
     LaunchMany(config, HeadlessDisplayer())
     if Exceptions:

commit 606b475a548d3d4d7f205cf74974436c13c2f26f

fmttime is local function, fix bug introduced in 1eacc1d

btlaunchmanycurses.py
@@ -178,7 +178,7 @@ class CursesDisplayer:
                     break
             (name, status, progress, peers, seeds, seedsmsg, dist,
              uprate, dnrate, upamt, dnamt, size, t, msg) = data[ii]
-            t = time.fmttime(t)
+            t = fmttime(t)
             if t:
                 status = t
             name = ljust(name, self.mainwinw - 32)
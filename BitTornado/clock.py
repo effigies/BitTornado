d8399835fc40212a4fc396ff0fb6c960f268ad5f
Christopher J. Markiewicz <effigies@gmail.com>
2015-02-08 10:56:19 -0500

    Study, understand, and clean up clock

--- BitTornado/clock.py
+++ BitTornado/clock.py
@@ -1,4 +1,10 @@
-"""Replicate win32 time.clock() behavior for all platforms"""
+"""Provide a non-decreasing clock() function.
+
+In Windows, time.clock() provides number of seconds from first call, so use
+that.
+
+In Unix, time.clock() is CPU time, and time.time() reports system time, which
+may not be non-decreasing."""
 
 import time
 import sys
@@ -7,24 +13,29 @@ _MAXFORWARD = 100
 _FUDGE = 1
 
 
-class RelativeTime:
+class RelativeTime(object): #pylint: disable=R0903
+    """Non-decreasing time implementation for Unix"""
     def __init__(self):
         self.time = time.time()
         self.offset = 0
 
     def get_time(self):
-        t = time.time() + self.offset
-        if t < self.time or t > self.time + _MAXFORWARD:
+        """Calculate a non-decreasing time representation"""
+        systemtime = time.time()
+
+        now = systemtime + self.offset
+
+        if self.time < now < self.time + _MAXFORWARD:
+            self.time = now
+        else:
+            # If time jump is outside acceptable bounds, move ahead one second
+            # and note the offset
             self.time += _FUDGE
-            self.offset += self.time - t
-            return self.time
-        self.time = t
-        return t
+            self.offset = self.time - systemtime
+
+        return self.time
 
 if sys.platform != 'win32':
-    _RTIME = RelativeTime()
-
-    def clock():
-        return _RTIME.get_time()
+    clock = RelativeTime().get_time     #pylint: disable=C0103
 else:
     from time import clock
